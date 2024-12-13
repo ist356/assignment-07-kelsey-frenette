0bb687c91a99402221e6eb6c518f4c90ed652eeb
kelsey frenette <kfrenett@syr.edu>
2024-12-13 17:47:14 -0500

    code change

--- code/menuitemextractor.py
+++ code/menuitemextractor.py
@@ -7,15 +7,41 @@ else:
 
 
 def clean_price(price:str) -> float:
-    pass # TODO  replace with code
+    price =price.replace ("<", "") # remove comma
+    price = price.replace ("$", "") # remove dollar sign
+    return float(price)
 
 def clean_scraped_text(scraped_text: str) -> list[str]:
-    pass # TODO  replace with code
+    items = scraped_text.split("\n") # split the text into lines
+    cleaned = []
+    for item in items:
+        if item in ['GS',"V","S","P"]: # skip these items
+            continue
+        if item.startswith("NEW"): # skip these items
+            continue
+        if len(item.strip()) == 0: # skip empty lines
+            continue
+
+        cleaned.append(item)
+    
+    return cleaned
 
 def extract_menu_item(title:str, scraped_text: str) -> MenuItem:
-    pass # TODO  replace with code
+    cleaned_items = clean_scraped_text(scraped_text)
+    item = MenuItem(category=title, name="", price=0.0, description="")
+    item.name = cleaned_items[0]
+    item.price = clean_price(cleaned_items[1])
+    if len(cleaned_items) > 2:
+        item.description = cleaned_items[2]
+    else:
+        item.description = "No description available."
+    return item
 
 
 
 if __name__=='__main__':
-    pass
+    test_items = []
+    title = "test"
+    for scraped_text in test_items:
+        item = extract_menu_item(title, scraped_text)
+        print(item)
--- code/tully_scraper.py
+++ code/tully_scraper.py
@@ -10,11 +10,27 @@ def tullyscraper(playwright: Playwright) -> None:
     page = context.new_page()
     page.goto("https://www.tullysgoodtimes.com/menus/")
 
-    # TODO Write code here
-    
-    # ---------------------
-    context.close()
-    browser.close()
+
+    extracted_items = []
+    for title in page.query_selector_all("h3.foodmenu__menu-section-title"):
+        title_text = title.inner_text().strip()
+        print(f"MENU SECTION: {title_text}")
+
+    row = title.query_selector("~ *").query_selector("~ *")
+    for item in row.query_selector_all("div.foodmenu__menu-item"):
+        item_text = item.inner_text().strip()
+        extracted_item = extract_menu_item(title_text, item_text)
+        print(f"  MENU ITEM: {extracted_item.name}")
+        
+        extracted_items.append(extracted_item.to_dict())
+
+        df = pd.DataFrame(extracted_items)
+        df.to_csv("cache/tullys_menu.csv", index=False)
+
+
+        context.close()
+        browser.close()
+
 
 
 with sync_playwright() as playwright:
